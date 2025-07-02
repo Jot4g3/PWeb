be2cbebb8623ec8463c360200d5aafe9d917cc44
Jot4g3 <gabriel.joao61@aluno.ifce.edu.br>
2025-07-01 21:08:21 -0300

    Consertando bugs em apinasa

--- Estudos/Exercícios/apinasa/app.js
+++ Estudos/Exercícios/apinasa/app.js
@@ -1,7 +1,7 @@
-const axios = require("axios")
-const express = require("express")
+const axios = require("axios") // Faz requisições HTTP
+const express = require("express") // Cria servidor web
 const app = express()
-require('dotenv').config()
+require('dotenv').config() // Carrega variáveis do .env
 const API_KEY = process.env.API_KEY
 
 if (!API_KEY) {
@@ -15,15 +15,21 @@ app.set("view engine", "ejs")
 const today = new Date();
 
 const year = today.getFullYear();
-const month = String(today.getMonth() + 1).padStart(2, '0'); // Mês é 0-indexado, então +1
+let year_foruse = year
+const month = String(today.getMonth() + 1).padStart(2, '0'); // Colocando o padStart para o 5 virar 05, por exemplo. +1 porque começa do zero!
+let month_foruse = month
+if (month - 1 == 0){
+  month_foruse = String(12).padStart(2, "0")
+  year_foruse = String(year-1).padStart(2, "0")
+}
 const day = String(today.getDate()).padStart(2, '0');
 
 console.log(`Hoje é: ${day}/${month}/${year}`);
 
 app.get("/", (req, res) => {
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&start_date=${year}-${(month - 1) == 0 ? 12 : (month - 1)}-${day}&end_date=${year}-${month}-${day}`)
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&start_date=${year_foruse}-${String(parseInt(month_foruse) - 1).padStart(2, '0')}-${day}&end_date=${year}-${month}-${day}`)
     .then(function (response) {
-        res.render("index", {data: response.data.filter(image => image.copyright == false)})
+        res.render("index", {data: response.data.filter(image => image.copyright == null)})
         console.log(response.data)
       })
       .catch(function (error) {
@@ -32,5 +38,5 @@ app.get("/", (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log("Running at port "+PORT)
+    console.log("Running at port " + PORT)
 })
